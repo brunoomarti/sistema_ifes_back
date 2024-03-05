7d2b4600c955aba632bb06968248152a6a6a1465
Bruno Martinelli <brunomarti5989@hotmail.com>
2024-03-05 16:49:20 -0300

    Initial commit

---
+++ src/test/java/com/sistemaifes/sistemaifes/SistemaifesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaifes.sistemaifes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaifesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
