05a2d1cba24ea3bfcbacfa8b0361a8f55e6f3e55
Bruno <brunogava2@gmail.com>
2024-03-07 17:11:19 -0300

    Estrutura inicial

---
+++ src/main/java/com/sistemaifes/sistemaifes/controller/EquipmentController.java
@@ -0,0 +1,5 @@
+package com.sistemaifes.sistemaifes.controller;
+
+public class EquipmentController {
+    
+}
---
+++ src/main/java/com/sistemaifes/sistemaifes/model/Equipment.java
@@ -0,0 +1,5 @@
+package com.sistemaifes.sistemaifes.model;
+
+public class Equipamento {
+    
+}
